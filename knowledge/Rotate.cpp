e5db2ff6baef67df4c89bae2c438e3e40b4ec6f7
Hades1123 <dp1.1a7.si@gmail.com>
2023-11-18 15:49:03 +0700

    hehe

--- knowledge/Rotate.cpp
+++ knowledge/Rotate.cpp
@@ -1,15 +1,29 @@
 #include<bits/stdc++.h>
+#include<vector>
 
 using namespace std;
 
+vector <int> even_num (vector<int> v)
+{
+    vector <int> res;
+    for (int i = 0; i < v.size() ; ++i)
+        if(v[i] % 2 == 0)
+            res.push_back(v[i]);
+    return res;
+}
 int main()
 {
     freopen("input.txt", "r", stdin);
     freopen("output.txt" , "w" , stdout);
 
-    int n = 100;
-    int* n_ptr = &n;
-    cout << n_ptr << endl;
-    cout << *n_ptr;
+    vector<int> v;
+    int x;
+    while(cin >> x)
+    {
+        v.push_back(x);
+    }
+    vector <int> res = even_num(v);
+    for (auto x:res)
+        cout << x << ' ';
     return 0;
 }
