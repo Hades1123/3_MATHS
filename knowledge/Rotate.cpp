54afbbcef93f9cf748566938ed3faa7d5f58cbb9
Hades1123 <dp1.1a7.si@gmail.com>
2023-11-18 15:17:39 +0700

    hehe

--- knowledge/Rotate.cpp
+++ knowledge/Rotate.cpp
@@ -4,8 +4,12 @@ using namespace std;
 
 int main()
 {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt" , "w" , stdout);
+
     int n = 100;
-    int* n_ptr = n;
-    cout << n_ptr;
+    int* n_ptr = &n;
+    cout << n_ptr << endl;
+    cout << *n_ptr;
     return 0;
 }
